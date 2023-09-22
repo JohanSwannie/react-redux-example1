c52194c44c56c7611b73d55a53bc77a99a3b9720
Johan Swan <jswannieman@gmail.com>
2023-09-22 14:14:32 +1200

    Update counterSlice Javascript Component

--- src/counters/counterSlice.js
+++ src/counters/counterSlice.js
@@ -12,7 +12,7 @@ export const counterSlice = createSlice({
       state.count += 1;
     },
     decrement: (state) => {
-      if (state.count > 10000) {
+      if ((state.count > 10000) | (state.count < 0)) {
         state.count = 50;
       } else {
         state.count -= 1;
