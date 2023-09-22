623bc2fed9575adde79ffd12286a3038d22c009d
Johan Swan <jswannieman@gmail.com>
2023-09-22 15:12:01 +1200

    Update Counter Javascript Component

--- src/counters/Counter.js
+++ src/counters/Counter.js
@@ -1,13 +1,29 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, division, multiplication } from "./counterSlice";
+import { useState } from "react";
+import {
+  increment,
+  decrement,
+  division,
+  multiplication,
+  reset,
+  inputIncrement,
+} from "./counterSlice";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+  const [inputIncrementAmount, setInputIncrementAmount] = useState(0);
+
+  const addInputAmount = Number(inputIncrementAmount) || 0;
+
+  const resetCounter = () => {
+    setInputIncrementAmount(0);
+    dispatch(reset());
+  };
 
   return (
     <section>
-      <p>Counter = {counter}</p>
+      <p id="display-counter">Counter = {counter}</p>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
@@ -18,6 +34,20 @@ const Counter = () => {
           <p>Subtract</p>
           <p>Divide</p>
           <p>Multiply</p>
+        </div>
+        <div className="input">
+          <label>Input Amount : </label>
+          <input
+            type="number"
+            value={inputIncrementAmount}
+            onChange={(event) => setInputIncrementAmount(event.target.value)}
+          />
+        </div>
+        <div className="extra-buttons">
+          <button onClick={() => dispatch(inputIncrement(addInputAmount))}>
+            Add Input Amount
+          </button>
+          <button onClick={resetCounter}>Reset</button>
         </div>
       </div>
     </section>
