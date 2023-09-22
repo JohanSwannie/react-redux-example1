225afc571d8e495e26d2cf7301cf211b890d027c
Johan Swan <jswannieman@gmail.com>
2023-09-22 12:42:20 +1200

    Update counterSlice Javascript Component

--- src/counters/counterSlice.js
+++ src/counters/counterSlice.js
@@ -14,8 +14,15 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.count -= 1;
     },
+    division: (state) => {
+      state.count /= 0.5;
+    },
+    multiplication: (state) => {
+      state.count *= 10;
+    },
   },
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, division, multiplication } =
+  counterSlice.actions;
 export default counterSlice.reducer;
