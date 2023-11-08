52969c295564d2b56e8cfe7506aea0b61b93778d
Johan Swan <jswannieman@gmail.com>
2023-11-09 11:39:42 +1300

    Update store JS Component

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,6 @@ import counterReducer from "../counters/counterSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counterMan: counterReducer,
   },
 });
